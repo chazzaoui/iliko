09934ef1603324c3f8105f04dbc6cab8f5012026
cceer <sisir.sital@gmail.com>
2023-01-29 12:26:55 +0545

    fix: security standard

--- src/components/Results/SubscribeForm.js
+++ src/components/Results/SubscribeForm.js
@@ -4,8 +4,7 @@ import CustomForm from "./CustomForm"
 import SmallText from "./SmallText"
 
 function SubscribeForm() {
-  const postUrl_prod = `https://live.us21.list-manage.com/subscribe/post?u=6d19a19539b819ba112e6f6c0&id=6046c6ddd6`
-  const postUrl = `https://gmail.us21.list-manage.com/subscribe/post?u=0a0c13f41d8a04cd71dfdaccc&id=ae0d2e921c`
+  
   //
   return (
     <div className="subscribeForm mt-[2rem] flex items-center justify-center flex-col lg:items-start">
